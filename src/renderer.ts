d85fbbb13b8e32965691518d493e53dcfb048fab
tom-nachum <tom.nachum@gmail.com>
2022-09-06 15:01:30 +0300

    rendered quote, pokemon, user and about with same pattern

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,13 +1,26 @@
 class Renderer {
   public render(user: User) {
-    this.renderFriends(user.friends);
+    this.renderHelper("friends", { friends: user.friends });
+    this.renderHelper("user", {
+      fname: user.fname,
+      lname: user.lname,
+      city: user.city,
+      state: user.state,
+      img: user.img,
+    });
+    this.renderHelper("quote", { quote: user.quote });
+    this.renderHelper("pokemon", {
+      pokemonName: user.pokemonName,
+      pokemonImg: user.pokemonImg,
+    });
+    this.renderHelper("meat", { about: user.about });
   }
 
-  private renderFriends(friends: Friend[]) {
-    $(".friends-container").empty();
-    const source = $("#friends-template").html();
+  private renderHelper(selector: string, data: any) {
+    $(`.${selector}-container`).empty();
+    const source = $(`#${selector}-template`).html();
     const template = Handlebars.compile(source);
-    const newHTML = template({ friends });
-    $(".friends-container").append(newHTML);
+    const newHTML = template(data);
+    $(`.${selector}-container`).append(newHTML);
   }
 }
