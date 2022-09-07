d05fea0a8d314d619a52c3bbb304be79d6297d0e
tom-nachum <tom.nachum@gmail.com>
2022-09-07 11:21:08 +0300

    enabled load button only when user is saved in local storage

--- src/ts-files/mvc/controller.ts
+++ src/ts-files/mvc/controller.ts
@@ -1,7 +1,13 @@
 (function () {
   const renderer = new Renderer();
   const model = new Model();
-  $("#save-user").prop("disabled", true);
+
+  const saveBtn = $("#save-user");
+  const loadBtn = $("#load-user");
+  saveBtn.prop("disabled", true);
+  if (Object.keys(JSON.parse(localStorage.user || "{}")).length === 0) {
+    loadBtn.prop("disabled", true);
+  }
 
   $("#generate-user").on("click", function () {
     model.fetchData().then(res => {
@@ -12,20 +18,21 @@
         model.getAbout(),
         model.getFriends()
       );
+      saveBtn.prop("disabled", false);
     });
-    $("#save-user").prop("disabled", false);
   });
 
   Handlebars.registerHelper("proper-case", function (pokemonName: string) {
     return pokemonName[0].toUpperCase() + pokemonName.slice(1);
   });
 
-  $("#save-user").on("click", function () {
+  saveBtn.on("click", function () {
     localStorage.user = JSON.stringify(model.getUser());
     alert("User saved successfully!");
+    loadBtn.prop("disabled", false);
   });
 
-  $("#load-user").on("click", function () {
-    console.log(JSON.parse(localStorage.user || "{}"));
+  loadBtn.on("click", function () {
+    const savedUser = JSON.parse(localStorage.user || "{}");
   });
 })();
